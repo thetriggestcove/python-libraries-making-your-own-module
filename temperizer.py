094bdb01b0fd725a408be2cc7a60fc13c720b822
Jessica Miles <jess.c.miles@gmail.com>
2021-02-26 15:32:34 -0500

    Complete everything except ec

--- temperizer.py
+++ temperizer.py
@@ -7,5 +7,24 @@ def convert_f_to_c(temperature_f):
 
 
 def convert_c_to_f(temperature_c):
-    """YOUR DOCSTRING HERE."""
-    pass
+    """Convert Celsius to Fahrenheit"""
+    temperature_f = (temperature_c * 9/5) + 32
+    return temperature_f
+
+def convert_c_to_k(temperature_c):
+    """Convert Celsius to Kelvin"""
+    return temperature_c + 273.15
+
+
+def convert_f_to_k(temperature_f):
+    """Convert Fahrenheit to Kelvin"""
+    return convert_f_to_c(temperature_f) + 273.15
+
+def convert_k_to_c(temperature_k):
+    """Convert Kelvin to Celsius."""
+    return temperature_k - 273.15
+
+
+def convert_k_to_f(temperature_k):
+    """Convert Kelvin to Fahrenheit"""
+    return convert_c_to_f(temperature_k - 273.15)
